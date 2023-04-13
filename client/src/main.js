630a3d1328a292260d34cc83462be58cad848f0a
Marco Rensch <marco.rensch@nx-designs.ch>
2023-04-13 10:30:13 +0200

    refactoring

--- client/src/main.js
+++ client/src/main.js
@@ -4,12 +4,12 @@ import "./registerServiceWorker";
 import router from "./router";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faUser);
 
 createApp(App)
-  .use(router)
   .component("font-awesome-icon", FontAwesomeIcon)
+  .use(router)
   .mount("#app");
