9160d922a2a881b70f7b625c1188f5522de0753a
Marco Rensch <marco.rensch@nx-designs.ch>
2023-05-01 11:30:02 +0200

    Manager permissions definiert

---
+++ client/src/views/brands/listView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="uk-section uk-section-small devices-list-view"
+    uk-height-viewport="offset-top:true"
+  >
+    <div class="uk-container">
+      <h1>Device Brands</h1>
+      <div
+        class="uk-width-1-1 uk-border-rounded uk-background-muted uk-padding-small"
+      >
+        <div class="uk-flex uk-flex-right">
+          <div v-if="authStore.hasPermission('can')">
+            <router-link
+              :to="{ name: 'create-device' }"
+              class="uk-button uk-button-primary"
+            >
+              <font-awesome-icon :icon="['fas', 'plus']" /> Add Device
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "listView",
+};
+</script>
+
+<style scoped></style>
