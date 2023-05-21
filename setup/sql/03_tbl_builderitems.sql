298448f5305378b7fb7dca8d8ecb67953385ebab
Marco Rensch <marco.rensch@nx-designs.ch>
2023-05-21 15:05:41 +0200

    Database Updates for TEXT columns

--- setup/sql/03_tbl_builderitems.sql
+++ setup/sql/03_tbl_builderitems.sql
@@ -5,9 +5,9 @@ CREATE TABLE IF NOT EXISTS `builder_items`
     category_id int(11)      NOT NULL,
     sorting     int(11)      NOT NULL,
     parent_id   int(11) DEFAULT NULL,
-    description text         NOT NULL,
-    hidden      text         NOT NULL,
-    params      text         NOT NULL,
+    description text    DEFAULT NULL,
+    hidden      text    DEFAULT NULL,
+    params      text    DEFAULT NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT `builder_items_categories` FOREIGN KEY (category_id) REFERENCES `builder_categories` (id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT `builder_items_parent` FOREIGN KEY (parent_id) REFERENCES `builder_items` (id) ON DELETE CASCADE ON UPDATE CASCADE
