f34252f000b4b2208c313f3e3da884d68a4dba8b
Marco Rensch <marco.rensch@nx-designs.ch>
2023-06-13 09:30:53 +0200

    Layouting

--- client/src/views/guides/GuideView.vue
+++ client/src/views/guides/GuideView.vue
@@ -7,7 +7,7 @@
       </div>
 
       <div class="uk-container uk-margin">
-        <div class="" uk-slider="center: true; finite:true">
+        <div id="guide-slider" class="" uk-slider="center: true; finite:true">
           <div class="uk-position-relative">
 
             <div class="uk-slider-container uk-visible-toggle" tabindex="-1">
@@ -17,13 +17,13 @@
                     <div class="">
                       <div class="uk-child-width-expand uk-grid-small" uk-grid>
                         <div>
-                          <div class="uk-padding" uk-scrollspy="target: > *; cls:uk-animation-slide-left-medium; delay:150; repeat:true;">
-                            <h3 class="uk-margin-remove">{{ si + 1 }}. {{ slide.name }}</h3>
-                            <div v-for="(step, index) of slide.content" :key="index">
+                          <div class="uk-padding">
+                            <h3 class="uk-margin-remove" uk-slider-parallax="x: 300,-300">{{ si + 1 }}. {{ slide.name }}</h3>
+                            <div v-for="(step, index) of slide.content" :key="index" uk-slider-parallax="x: 400,-400">
                               <p class="uk-margin-remove uk-text-bold">{{ step.label }}</p>
                               <p class="uk-margin-remove">{{ step.description }}</p>
                             </div>
-                            <div class="uk-margin-top">
+                            <div class="uk-margin-top" uk-slider-parallax="x: 450,-450">
                               <button v-if="si < slides.length - 1" class="uk-button uk-button-secondary uk-width-1-1"
                                       uk-slider-item="next">
                                 Next
@@ -35,9 +35,25 @@
                           </div>
                         </div>
                         <div>
-                          <div class="uk-position-relative uk-flex uk-flex-center">
-                            <div class="uk-border-rounded uk-overflow-hidden" uk-scrollspy="cls:uk-animation-slide-right-medium;delay:100;repeat:true;">
-                              <img class="slide-image" :src="'/public/' + slide.uri" :alt="slide.name">
+                          <div class="uk-flex uk-flex-center" >
+                            <div class="uk-border-rounded uk-overflow-hidden uk-position-relative">
+                              <img :id="'slide-image-' + si" :data-index="si" class="slide-image" :src="'/public' + slide.uri" :alt="slide.name">
+                              <div class="canvas-container uk-position-cover">
+                                <v-stage :config="configKonva[si]">
+                                  <v-layer>
+                                  <template v-for="shape in slide.content" :key="shape.id">
+                                    <v-circle
+                                        v-if="shape.type === 'circle'"
+                                        :config="shape"
+                                    ></v-circle>
+                                    <v-rect
+                                        v-if="shape.type === 'rectangle'"
+                                        :config="shape"
+                                    ></v-rect>
+                                  </template>
+                                  </v-layer>
+                                </v-stage>
+                              </div>
                             </div>
                           </div>
                         </div>
@@ -49,15 +65,18 @@
             </div>
 
             <div class="uk-hidden@s uk-light">
-              <a class="uk-position-center-left uk-position-small" href="#" uk-slidenav-previous uk-slider-item="previous">
+              <a class="uk-position-center-left uk-position-small" href="#" uk-slidenav-previous
+                 uk-slider-item="previous">
               </a>
               <a class="uk-position-center-right uk-position-small" href="#" uk-slidenav-next uk-slider-item="next"></a>
             </div>
 
             <div class="uk-visible@s">
-              <a class="uk-position-center-left-out uk-position-small" href="#" uk-slidenav-previous uk-slider-item="previous">
+              <a class="uk-position-center-left-out uk-position-small" href="#" uk-slidenav-previous
+                 uk-slider-item="previous">
               </a>
-              <a class="uk-position-center-right-out uk-position-small" href="#" uk-slidenav-next uk-slider-item="next"></a>
+              <a class="uk-position-center-right-out uk-position-small" href="#" uk-slidenav-next
+                 uk-slider-item="next"></a>
             </div>
 
           </div>
@@ -73,6 +92,7 @@
 <script>
 
 import axios from "axios";
+import UIkit from "uikit";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
 export default {
@@ -80,14 +100,42 @@ export default {
   components: {FontAwesomeIcon},
   data() {
     return {
+      imageLoaded: false,
       guide: {},
       slides: [],
+      configKonva: {},
+      currentSlideDomElement: null,
+      currentSlideIndex: 0,
     }
+  },
+
+  beforeUnmount() {
+    window.removeEventListener('resize', this.handleWindowResize);
   },
   async mounted() {
     await this.getGuide();
+
+    window.addEventListener('resize', this.handleWindowResize);
+
+    UIkit.util.on('#guide-slider', 'itemshown', (e) => {
+      this.currentSlideDomElement = e.target;
+      this.currentImageDomElement = this.currentSlideDomElement.querySelector('img.slide-image');
+      if(!this.currentImageDomElement.complete) {
+        this.loadImage(this.currentImageDomElement).then((imageDomElement) => {
+          this.initStage(imageDomElement)
+        });
+      }else {
+        this.initStage(this.currentImageDomElement)
+      }
+    });
+
   },
   methods: {
+    handleWindowResize() {
+      this.$nextTick(() => {
+        this.initStage(this.currentImageDomElement)
+      });
+    },
     async getGuide() {
       try {
         const response = await axios.get(`/api/guides/${this.$route.params.id}`);
@@ -100,11 +148,50 @@ export default {
           }
           return slide;
         });
-        console.log(this.slides);
       } catch (error) {
         console.log(error);
       }
-    }
+    },
+
+    async loadImage(element){
+      return new Promise((resolve, reject) => {
+        element.onload = () => {
+          resolve(element);
+        };
+        element.onerror = reject;
+      });
+    },
+
+    initStage(imageDomElement){
+      this.currentSlideIndex = imageDomElement.dataset.index;
+      this.configKonva[this.currentSlideIndex] = {
+        width: imageDomElement.offsetWidth,
+        height: imageDomElement.offsetHeight
+      };
+      this.placeShapes(this.currentSlideIndex);
+    },
+
+    placeShapes(stageIndex){
+      const slide = this.slides[stageIndex];
+      const shapes = slide.content;
+      shapes.forEach(shape => {
+        this.updateShapeDimensionsByPercentages(shape, stageIndex)
+      });
+    },
+    updateShapeDimensionsByPercentages(shape, stageIndex) {
+      shape.x = (this.configKonva[stageIndex].width / 100) * shape.percentages.x;
+      shape.y = (this.configKonva[stageIndex].height / 100) * shape.percentages.y;
+      shape.strokeWidth = Math.ceil((this.configKonva[stageIndex].width / 100) * shape.percentages.strokeWidth);
+      if (shape.type === "rectangle") {
+        shape.width = (this.configKonva[stageIndex].width / 100) * shape.percentages.width;
+        shape.height = (this.configKonva[stageIndex].height / 100) * shape.percentages.height;
+        shape.cornerRadius = (this.configKonva[stageIndex].width / 100) * shape.percentages.cornerRadius;
+      }
+      if (shape.type === "circle") {
+        shape.radius = (this.configKonva[stageIndex].width / 100) * shape.percentages.radius;
+      }
+
+    },
   }
 }
 </script>
