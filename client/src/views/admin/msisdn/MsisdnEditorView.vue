ef7bafa136c59eb7438ac64cf04dbd42a2780972
Marco Rensch <marco.rensch@nx-designs.ch>
2023-06-19 14:58:44 +0200

    GUI Bug - Async wurde nicht awaited

--- client/src/views/admin/msisdn/MsisdnEditorView.vue
+++ client/src/views/admin/msisdn/MsisdnEditorView.vue
@@ -7,46 +7,46 @@
         <div class="uk-card-body">
           <div class="uk-child-width-1-1 uk-child-width-1-2@m" uk-grid>
             <div
-              :class="{
+                :class="{
                 'uk-disabled nxd-disabled-section': /\d+/.exec(item.parent_id),
               }"
             >
               <label for="abo">Abonnement</label>
               <input
-                type="text"
-                id="abo"
-                class="uk-input"
-                v-model="item.abonnement"
-                :class="{ 'form-invalid': v$.item.abonnement.$errors.length }"
+                  type="text"
+                  id="abo"
+                  class="uk-input"
+                  v-model="item.abonnement"
+                  :class="{ 'form-invalid': v$.item.abonnement.$errors.length }"
               />
               <div
-                v-if="v$.item.abonnement.$errors.length"
-                class="uk-text-danger uk-text-small"
+                  v-if="v$.item.abonnement.$errors.length"
+                  class="uk-text-danger uk-text-small"
               >
                 <div
-                  v-for="error in v$.item.abonnement.$errors"
-                  :key="error.id"
+                    v-for="error in v$.item.abonnement.$errors"
+                    :key="error.id"
                 >
                   {{ error.$message }}
                 </div>
               </div>
             </div>
             <div
-              :class="{
+                :class="{
                 'uk-disabled nxd-disabled-section': /\d+/.exec(item.parent_id),
               }"
             >
               <label for="msisdn">SCN</label>
               <input
-                type="text"
-                id="scn"
-                class="uk-input"
-                v-model="item.scn"
-                :class="{ 'form-invalid': v$.item.scn.$errors.length }"
+                  type="text"
+                  id="scn"
+                  class="uk-input"
+                  v-model="item.scn"
+                  :class="{ 'form-invalid': v$.item.scn.$errors.length }"
               />
               <div
-                v-if="v$.item.scn.$errors.length"
-                class="uk-text-danger uk-text-small"
+                  v-if="v$.item.scn.$errors.length"
+                  class="uk-text-danger uk-text-small"
               >
                 <div v-for="error in v$.item.scn.$errors" :key="error.id">
                   {{ error.$message }}
@@ -56,16 +56,16 @@
             <div>
               <label for="msisdn">{{ techString }}MSISDN</label>
               <input
-                type="text"
-                id="msisdn"
-                class="uk-input"
-                placeholder="4179..."
-                v-model="item.msisdn"
-                :class="{ 'form-invalid': v$.item.msisdn.$errors.length }"
+                  type="text"
+                  id="msisdn"
+                  class="uk-input"
+                  placeholder="4179..."
+                  v-model="item.msisdn"
+                  :class="{ 'form-invalid': v$.item.msisdn.$errors.length }"
               />
               <div
-                v-if="v$.item.msisdn.$errors.length"
-                class="uk-text-danger uk-text-small"
+                  v-if="v$.item.msisdn.$errors.length"
+                  class="uk-text-danger uk-text-small"
               >
                 <div v-for="error in v$.item.msisdn.$errors" :key="error.id">
                   {{ error.$message }}
@@ -77,9 +77,9 @@
               <select id="parent_id" class="uk-select" v-model="item.parent_id">
                 <option value="null">Main MSISDN</option>
                 <option
-                  v-for="parent of parent_msisdns"
-                  :value="parent.id"
-                  :key="parent.id"
+                    v-for="parent of parent_msisdns"
+                    :value="parent.id"
+                    :key="parent.id"
                 >
                   {{ parent.msisdn }} ({{ parent.abonnement || "No Abo" }})
                 </option>
@@ -88,20 +88,20 @@
             <div>
               <label for="sim_number">SIM Number</label>
               <input
-                type="text"
-                id="sim_number"
-                class="uk-input"
-                placeholder="894101..."
-                v-model="item.sim_number"
-                :class="{ 'form-invalid': v$.item.sim_number.$errors.length }"
+                  type="text"
+                  id="sim_number"
+                  class="uk-input"
+                  placeholder="894101..."
+                  v-model="item.sim_number"
+                  :class="{ 'form-invalid': v$.item.sim_number.$errors.length }"
               />
               <div
-                v-if="v$.item.sim_number.$errors.length"
-                class="uk-text-danger uk-text-small"
+                  v-if="v$.item.sim_number.$errors.length"
+                  class="uk-text-danger uk-text-small"
               >
                 <div
-                  v-for="error in v$.item.sim_number.$errors"
-                  :key="error.id"
+                    v-for="error in v$.item.sim_number.$errors"
+                    :key="error.id"
                 >
                   {{ error.$message }}
                 </div>
@@ -110,26 +110,26 @@
             <div>
               <label for="sim_type">SIM Type</label>
               <select
-                id="sim_type"
-                class="uk-select"
-                v-model="item.sim_type_id"
+                  id="sim_type"
+                  class="uk-select"
+                  v-model="item.sim_type_id"
               >
                 <option value="null">Select</option>
                 <option
-                  v-for="type in sim_types"
-                  :key="type.id"
-                  :value="type.id"
+                    v-for="type in sim_types"
+                    :key="type.id"
+                    :value="type.id"
                 >
                   {{ type.name }}
                 </option>
               </select>
               <div
-                v-if="v$.item.sim_type_id.$errors.length"
-                class="uk-text-danger uk-text-small"
+                  v-if="v$.item.sim_type_id.$errors.length"
+                  class="uk-text-danger uk-text-small"
               >
                 <div
-                  v-for="error in v$.item.sim_type_id.$errors"
-                  :key="error.id"
+                    v-for="error in v$.item.sim_type_id.$errors"
+                    :key="error.id"
                 >
                   {{ error.$message }}
                 </div>
@@ -143,62 +143,62 @@
           <div class="uk-margin">
             <label for="notes">Notes</label>
             <textarea
-              id="notes"
-              class="uk-textarea"
-              rows="5"
-              placeholder="Notes"
-              v-model="item.notes"
+                id="notes"
+                class="uk-textarea"
+                rows="5"
+                placeholder="Notes"
+                v-model="item.notes"
             ></textarea>
           </div>
           <div class="uk-margin">
             <label for="notes">Hidden</label>
             <textarea
-              id="hidden"
-              class="uk-textarea"
-              rows="5"
-              placeholder="Hidden Notes"
-              v-model="item.hidden"
+                id="hidden"
+                class="uk-textarea"
+                rows="5"
+                placeholder="Hidden Notes"
+                v-model="item.hidden"
             ></textarea>
           </div>
         </div>
       </div>
     </div>
     <ControlsFooterWidget
-      :canDelete="item.id"
-      @cancel="handleCancelClicked"
-      @save="handleSaveClicked"
-      @delete="handleDeleteClicked"
+        :canDelete="item.id"
+        @cancel="handleCancelClicked"
+        @save="handleSaveClicked"
+        @delete="handleDeleteClicked"
     />
   </div>
 </template>
 
 <script>
-import { useToast } from "vue-toastification";
+import {useToast} from "vue-toastification";
 import UIkit from "uikit";
 import ControlsFooterWidget from "@/components/ControlsFooterWidget.vue";
 import axios from "axios";
-import { useVuelidate } from "@vuelidate/core";
-import { helpers, requiredIf, numeric } from "@vuelidate/validators";
+import {useVuelidate} from "@vuelidate/core";
+import {helpers, requiredIf, numeric} from "@vuelidate/validators";
 
 const exactLength = (length, fieldname = "") =>
-  helpers.withMessage(
-    `${fieldname} must be ${length} digits`,
-    (value) => value.length === length
-  );
+    helpers.withMessage(
+        `${fieldname} must be ${length} digits`,
+        (value) => value.length === length
+    );
 
 const startsWith = (prefix, fieldname = "") =>
-  helpers.withMessage(`${fieldname} must start with ${prefix}`, (value) =>
-    value.startsWith(prefix)
-  );
+    helpers.withMessage(`${fieldname} must start with ${prefix}`, (value) =>
+        value.startsWith(prefix)
+    );
 
 const validSelection = (fieldname = "") => {
   return helpers.withMessage(`Please select a ${fieldname}`, (value) => {
     return (
-      value !== "null" &&
-      value !== null &&
-      value !== undefined &&
-      value !== "" &&
-      value !== 0
+        value !== "null" &&
+        value !== null &&
+        value !== undefined &&
+        value !== "" &&
+        value !== 0
     );
   });
 };
@@ -208,9 +208,9 @@ export default {
   setup() {
     const toast = useToast();
     const v$ = useVuelidate();
-    return { toast, v$ };
+    return {toast, v$};
   },
-  components: { ControlsFooterWidget },
+  components: {ControlsFooterWidget},
   validations() {
     return {
       item: {
@@ -226,10 +226,10 @@ export default {
         },
         abonnement: {
           requiredIfMain: helpers.withMessage(
-            "Abonnement Description is required for Main MSISDN",
-            requiredIf(function () {
-              return !/\d+/.exec(this.item.parent_id);
-            })
+              "Abonnement Description is required for Main MSISDN",
+              requiredIf(function () {
+                return !/\d+/.exec(this.item.parent_id);
+              })
           ),
         },
         sim_type_id: {
@@ -272,7 +272,7 @@ export default {
     this.getParentMsisdns();
     if (this.id) {
       await this.getMsisdnData();
-      if(!this.item || !this.item.id){
+      if (!this.item || !this.item.id) {
         this.toast.error("MSISDN not found");
         this.$router.push("/admin");
       }
@@ -280,47 +280,45 @@ export default {
   },
   methods: {
     getMsisdnData() {
-      axios
-        .get("/api/admin/msisdns/" + this.id)
-        .then((response) => {
-          console.log(response.data);
-          this.item = response.data;
-        })
-        .catch((error) => {
-          console.log(error.response);
-          this.$router.push("/");
-        });
+      return axios.get("/api/admin/msisdns/" + this.id)
+          .then((response) => {
+            this.item = response.data;
+          })
+          .catch((error) => {
+            console.log(error.response);
+            this.$router.push("/");
+          });
     },
     getParentMsisdns() {
       axios
-        .get("/api/admin/msisdns", {
-          params: {
-            parentOnly: true,
-          },
-        })
-        .then((response) => {
-          console.log(response.data);
-          this.parent_msisdns = response.data.filter((item) => {
-            if (parseInt(item.id) !== parseInt(this.id)) return true;
+          .get("/api/admin/msisdns", {
+            params: {
+              parentOnly: true,
+            },
+          })
+          .then((response) => {
+            console.log(response.data);
+            this.parent_msisdns = response.data.filter((item) => {
+              if (parseInt(item.id) !== parseInt(this.id)) return true;
+            });
+          })
+          .catch((error) => {
+            console.log(error);
           });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
     },
     getSimTypes() {
       axios
-        .get("/api/admin/sim_types")
-        .then((response) => {
-          console.log(response.data);
-          this.sim_types = response.data;
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+          .get("/api/admin/sim_types")
+          .then((response) => {
+            console.log(response.data);
+            this.sim_types = response.data;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
     },
     handleCancelClicked() {
-      this.$router.push({ name: "msisdn-manager" });
+      this.$router.push({name: "msisdn-manager"});
     },
     async handleSaveClicked() {
       const formIsValid = await this.v$.$validate();
@@ -342,28 +340,28 @@ export default {
     },
     saveNewMsisdn() {
       axios
-        .post("/api/admin/msisdns", this.item)
-        .then((res) => {
-          this.handleResponse(res);
-        })
-        .catch((error) => {
-          this.handleResponse(error.response);
-        });
+          .post("/api/admin/msisdns", this.item)
+          .then((res) => {
+            this.handleResponse(res);
+          })
+          .catch((error) => {
+            this.handleResponse(error.response);
+          });
     },
     updateMsisdn() {
       axios
-        .put("/api/admin/msisdns/" + this.id, this.item)
-        .then(() => {
-          this.$router.push({ name: "msisdn-manager" });
-        })
-        .catch((error) => {
-          this.handleResponse(error.response);
-        });
+          .put("/api/admin/msisdns/" + this.id, this.item)
+          .then(() => {
+            this.$router.push({name: "msisdn-manager"});
+          })
+          .catch((error) => {
+            this.handleResponse(error.response);
+          });
     },
     deleteMsisdn() {
       axios.delete("/api/admin/msisdns/" + this.id)
           .then(() => {
-            this.$router.push({ name: "msisdn-manager" });
+            this.$router.push({name: "msisdn-manager"});
           })
           .catch((error) => {
             console.log(error)
@@ -372,19 +370,20 @@ export default {
     },
     handleDeleteClicked() {
       UIkit.modal
-        .confirm("Are you sure you want to delete this MSISDN?")
-        .then(() => {
-          this.deleteMsisdn();
-        }, () => {});
+          .confirm("Are you sure you want to delete this MSISDN?")
+          .then(() => {
+            this.deleteMsisdn();
+          }, () => {
+          });
     },
     handleResponse(response) {
       if (response.status === 200 || response.status === 201) {
-        this.$router.push({ name: "msisdn-manager" });
+        this.$router.push({name: "msisdn-manager"});
         this.toast.success("MSISDN saved successfully");
       } else {
         this.toast.error("Error saving MSISDN");
         console.error(
-          `${response.status} - ${response.statusText}: ${response.data}`
+            `${response.status} - ${response.statusText}: ${response.data}`
         );
       }
     },
